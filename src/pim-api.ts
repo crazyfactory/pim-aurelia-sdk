bb84ba7fa46ca8a1762f6e0d3c2b604bab29c5fc
wmathes <fanatik@gmx.at>
2016-04-12 11:07:58 +0700

    WIP: generated data test

--- src/pim-api.ts
+++ src/pim-api.ts
@@ -2,58 +2,159 @@
    The contents of this file are automatically generated
 
    Version: DEV
-   Date:    4/11/2016 12:05:24
+   Date:    4/12/2016 11:06:51
 */
 
 import {BaseApi} from './pim-api-core';
 import {ICountryData, ICurrencyData, ICurrencyExchangeRateData, IPieceAttributeData, IPieceGroupData, IPieceGroupTypeData, IPagedResultData, IPieceData, IPieceSelectorData} from 'pim-data';
 
 export class CountriesApi extends BaseApi {
-    public Create(data: ICountryData):Promise<ICountryData> {
+    public create(data: ICountryData): Promise<ICountryData> {
         return this._put(`api/countries`, data);
     }
 
-    public Delete(key:string):Promise<boolean> {
+    public delete(key: string): Promise<boolean> {
         return this._delete(`api/countries`, key);
     }
 
-    public Get(key:string):Promise<ICountryData> {
+    public get(key: string): Promise<ICountryData> {
         return this._get(`api/countries/${key}`);
     }
 
-    public GetAll():Promise<ICountryData[]> {
+    public getAll(): Promise<ICountryData[]> {
         return this._get(`api/countries`);
     }
 
-    public Update(data:ICountryData):Promise<ICountryData> {
+    public update(data: ICountryData): Promise<ICountryData> {
         return this._patch(`api/countries`, data);
     }
 }
 
 export class CurrenciesApi extends BaseApi {
-    public Create(data:ICurrencyData):Promise<ICurrencyData> {
+    public create(data: ICurrencyData): Promise<ICurrencyData> {
         return this._put(`api/currencies`, data);
     }
 
-    public Delete(key:string):Promise<boolean> {
+    public delete(key: string): Promise<boolean> {
         return this._delete(`api/currencies`, key);
     }
 
-    public Get(key:string):Promise<ICurrencyData> {
+    public get(key: string): Promise<ICurrencyData> {
         return this._get(`api/currencies/${key}`);
     }
 
-    public GetAll():Promise<ICurrencyData[]> {
+    public getAll(): Promise<ICurrencyData[]> {
         return this._get(`api/currencies`);
     }
 
-    public GetRate(key:string, date:string):Promise<ICurrencyExchangeRateData> {
+    public getRate(key: string, date: string): Promise<ICurrencyExchangeRateData> {
         return this._get(`api/currencies/${key}/rates/${date}`);
     }
 
-    public Update(data:ICurrencyData):Promise<ICurrencyData> {
+    public update(data: ICurrencyData): Promise<ICurrencyData> {
         return this._patch(`api/currencies`, data);
     }
 }
+
+export class PieceAttributesApi extends BaseApi {
+    public addValue(key: string, value: string): Promise<IPieceAttributeData> {
+        return this._put(`api/pieceattributes/${key}/values`, value);
+    }
+
+    public deleteValue(key: string, value: string): Promise<boolean> {
+        return this._delete(`api/pieceattributes/${key}/values`, value);
+    }
+
+    public get(key: string): Promise<IPieceAttributeData> {
+        return this._get(`api/pieceattributes/${key}`);
+    }
+
+    public getAll(): Promise<IPieceAttributeData[]> {
+        return this._get(`api/pieceattributes`);
+    }
+
+    public update(key: string, data: IPieceAttributeData): Promise<IPieceAttributeData> {
+        return this._patch(`api/pieceattributes/${key}`, data);
+    }
+}
+
+export class PieceGroupsApi extends BaseApi {
+    public create(data: IPieceGroupData): Promise<IPieceGroupData> {
+        return this._put(`api/piecegroups`, data);
+    }
+
+    public delete(id: number): Promise<boolean> {
+        return this._delete(`api/piecegroups`, null);
+    }
+
+    public get(id: number): Promise<IPieceGroupData> {
+        return this._get(`api/piecegroups/${id}`);
+    }
+
+    public getAll(): Promise<IPieceGroupData[]> {
+        return this._get(`api/piecegroups`);
+    }
+
+    public getByCode(code: string): Promise<IPieceGroupData> {
+        return this._get(`api/piecegroups/by-code/${code}`);
+    }
+
+    public update(data: IPieceGroupData): Promise<IPieceGroupData> {
+        return this._patch(`api/piecegroups`, data);
+    }
+}
+
+export class PieceGroupTypesApi extends BaseApi {
+    public create(data: IPieceGroupTypeData): Promise<IPieceGroupTypeData> {
+        return this._put(`api/piecegrouptypes`, data);
+    }
+
+    public delete(id: number): Promise<boolean> {
+        return this._delete(`api/piecegrouptypes`, id);
+    }
+
+    public get(id: number): Promise<IPieceGroupTypeData> {
+        return this._get(`api/piecegrouptypes/${id}`);
+    }
+
+    public getList(): Promise<IPieceGroupTypeData[]> {
+        return this._get(`api/piecegrouptypes`);
+    }
+
+    public update(data: IPieceGroupTypeData): Promise<IPieceGroupTypeData> {
+        return this._patch(`api/piecegrouptypes`, data);
+    }
+}
+
+export class PiecesApi extends BaseApi {
+    public count(data: IPieceSelectorData): Promise<number> {
+        return this._head(`api/pieces`, data);
+    }
+
+    public create(data: IPieceData): Promise<IPieceData> {
+        return this._put(`api/pieces`, data);
+    }
+
+    public delete(id: number): Promise<IPieceData> {
+        return this._delete(`api/pieces`, id);
+    }
+
+    public find(data: IPieceSelectorData): Promise<IPagedResultData<IPieceData>> {
+        return this._post(`api/pieces`, data);
+    }
+
+    public get(id: number): Promise<IPieceData> {
+        return this._get(`api/pieces/${id}`);
+    }
+
+    public getByCode(code: string): Promise<IPieceData> {
+        return this._get(`api/pieces/by-code/${code}`);
+    }
+
+    public update(data: IPieceData): Promise<IPieceData> {
+        return this._patch(`api/pieces`, data);
+    }
+}
+
 
 /* End of file */
--- src/typings/pim-api.d.ts
+++ src/typings/pim-api.d.ts
@@ -1,29 +1,100 @@
 /*
- The contents of this file are automatically generated
+   The contents of this file are automatically generated
 
- Version: DEV
- Date:    4/11/2016 12:05:24
- */
+   Version: DEV
+   Date:    4/12/2016 11:06:51
+*/
 
 declare module 'pim-api' {
+    import {IApiConfiguration} from 'pim-core';
+    import {ICountryData, ICurrencyData, ICurrencyExchangeRateData, IPieceAttributeData, IPieceGroupData, IPieceGroupTypeData, IPagedResultData, IPieceData, IPieceSelectorData} from 'pim-data';
 
-	import {ICountryData, ICurrencyData, ICurrencyExchangeRateData, IPieceAttributeData, IPieceGroupData, IPieceGroupTypeData, IPagedResultData, IPieceData, IPieceSelectorData} from 'pim-data';
-	import {IApiConfiguration} from 'pim-core';
+    export class CountriesApi {
+        constructor(config?: IApiConfiguration);
+        create(data: ICountryData): Promise<ICountryData>;
+
+        delete(key: string): Promise<boolean>;
+
+        get(key: string): Promise<ICountryData>;
+
+        getAll(): Promise<ICountryData[]>;
+
+        update(data: ICountryData): Promise<ICountryData>;
+    }
+
+    export class CurrenciesApi {
+        constructor(config?: IApiConfiguration);
+        create(data: ICurrencyData): Promise<ICurrencyData>;
+
+        delete(key: string): Promise<boolean>;
+
+        get(key: string): Promise<ICurrencyData>;
+
+        getAll(): Promise<ICurrencyData[]>;
+
+        getRate(key: string, date: string): Promise<ICurrencyExchangeRateData>;
+
+        update(data: ICurrencyData): Promise<ICurrencyData>;
+    }
+
+    export class PieceAttributesApi {
+        constructor(config?: IApiConfiguration);
+        addValue(key: string, value: string): Promise<IPieceAttributeData>;
+
+        deleteValue(key: string, value: string): Promise<boolean>;
+
+        get(key: string): Promise<IPieceAttributeData>;
+
+        getAll(): Promise<IPieceAttributeData[]>;
+
+        update(key: string, data: IPieceAttributeData): Promise<IPieceAttributeData>;
+    }
+
+    export class PieceGroupsApi {
+        constructor(config?: IApiConfiguration);
+        create(data: IPieceGroupData): Promise<IPieceGroupData>;
+
+        delete(id: number): Promise<boolean>;
+
+        get(id: number): Promise<IPieceGroupData>;
+
+        getAll(): Promise<IPieceGroupData[]>;
+
+        getByCode(code: string): Promise<IPieceGroupData>;
+
+        update(data: IPieceGroupData): Promise<IPieceGroupData>;
+    }
+
+    export class PieceGroupTypesApi {
+        constructor(config?: IApiConfiguration);
+        create(data: IPieceGroupTypeData): Promise<IPieceGroupTypeData>;
+
+        delete(id: number): Promise<boolean>;
+
+        get(id: number): Promise<IPieceGroupTypeData>;
+
+        getList(): Promise<IPieceGroupTypeData[]>;
+
+        update(data: IPieceGroupTypeData): Promise<IPieceGroupTypeData>;
+    }
+
+    export class PiecesApi {
+        constructor(config?: IApiConfiguration);
+        count(data: IPieceSelectorData): Promise<number>;
+
+        create(data: IPieceData): Promise<IPieceData>;
+
+        delete(id: number): Promise<IPieceData>;
+
+        find(data: IPieceSelectorData): Promise<IPagedResultData<IPieceData>>;
+
+        get(id: number): Promise<IPieceData>;
+
+        getByCode(code: string): Promise<IPieceData>;
+
+        update(data: IPieceData): Promise<IPieceData>;
+    }
 
 
-	export class CountriesApi {
-		constructor(config?: ICountryData);
-
-		Create(data: ICountryData): Promise<ICountryData>;
-
-		Delete(key: string): Promise<boolean>;
-
-		Get(key: string): Promise<ICountryData>;
-
-		GetAll(): Promise<ICountryData[]>;
-
-		Update(data: ICountryData): Promise<ICountryData>;
-	}
 }
-
 /* End of file */
